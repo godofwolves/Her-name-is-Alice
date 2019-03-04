ca7f8873ba77e7285dfafc475c54234784bd764d
aubrin cole <dcolwell@juno.com>
2019-03-04 13:20:16 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+// "el" stands for the "element" that was just clicked
+/*function addClass(el){
+	el.classList.add("opaque");
+}
+
+*/
+// "el" stands for the "element" that was just clicked
+// password: 
+function check() {
+  x = document.getElementById("myPwd").value;
+  if (x !== "heart") {
+    alert("Try a different path");
+    return false;
+  }else {
+window.location = "https://www.google.com";
+  }
+}
+
+function key()	{
+  if (document.getElementById("hehe").src.match("images/y.png")) {
+alert("I already gave you a hint");
+  }else { 
+    	document.getElementById("hehe").src = "images/y.png";
+  }
+}
